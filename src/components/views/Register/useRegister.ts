c39724b53d1acee88027fd706457556b128daa3a
christ-saragih <bennefit.19@gmail.com>
2025-03-03 20:29:35 +0700

    feat: registration feature with UI components and password visibility toggle

---
+++ src/components/views/Register/useRegister.ts
@@ -0,0 +1,21 @@
+// logic aplikasi fitur register
+
+import { useState } from "react";
+
+const useRegister = () => {
+  const [visiblePassword, setVisiblePassword] = useState({
+    password: false,
+    passwordConfirmation: false,
+  });
+
+  const handleVisiblePassword = (key: "password" | "passwordConfirmation") => {
+    setVisiblePassword({
+      ...visiblePassword,
+      [key]: !visiblePassword[key],
+    });
+  };
+
+  return { visiblePassword, handleVisiblePassword };
+};
+
+export default useRegister;
---
+++ src/pages/auth/register/index.tsx
@@ -0,0 +1,12 @@
+import AuthLayout from "@/components/layouts/AuthLayouts";
+import Register from "@/components/views/Register";
+
+const RegisterPage = () => {
+  return (
+    <AuthLayout title="Eventure | Register">
+      <Register />
+    </AuthLayout>
+  );
+};
+
+export default RegisterPage;
