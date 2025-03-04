799ea82bcfa1229ffcb370fa2dd75d3533183f2f
christ-saragih <bennefit.19@gmail.com>
2025-03-04 21:42:42 +0700

    feat: add authentication service and registration form with validation

--- src/components/views/Register/useRegister.ts
+++ src/components/views/Register/useRegister.ts
@@ -1,21 +1,86 @@
 // logic aplikasi fitur register
-
 import { useState } from "react";
+import * as yup from "yup";
+import { set, useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { IRegister } from "@/types/Auth";
+import authServices from "@/services/auth";
+import { useMutation } from "@tanstack/react-query";
+import { useRouter } from "next/router";
+
+const registerSchema = yup.object().shape({
+  fullName: yup.string().required("Please enter your full name"),
+  username: yup.string().required("Please enter your username"),
+  email: yup
+    .string()
+    .email("Email format not valid")
+    .required("Please enter your email"),
+  password: yup
+    .string()
+    .min(8, "Minimal 8 characters")
+    .required("Please enter your password"),
+  confirmPassword: yup
+    .string()
+    .oneOf([yup.ref("password")], "Confirm password does not match")
+    .required("Please enter your confirm password"),
+});
 
 const useRegister = () => {
+  const router = useRouter();
+
   const [visiblePassword, setVisiblePassword] = useState({
     password: false,
-    passwordConfirmation: false,
+    confirmPassword: false,
   });
 
-  const handleVisiblePassword = (key: "password" | "passwordConfirmation") => {
+  const handleVisiblePassword = (key: "password" | "confirmPassword") => {
     setVisiblePassword({
       ...visiblePassword,
       [key]: !visiblePassword[key],
     });
   };
 
-  return { visiblePassword, handleVisiblePassword };
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+    reset,
+    setError,
+  } = useForm({
+    resolver: yupResolver(registerSchema),
+  });
+
+  const registerService = async (payload: IRegister) => {
+    const result = await authServices.register(payload);
+    return result;
+  };
+
+  const { mutate: mutateRegister, isPending: isPendingRegister } = useMutation({
+    mutationFn: registerService,
+    onError: (error) => {
+      setError("root", {
+        message: error.message,
+      });
+    },
+    onSuccess: () => {
+      router.push("/auth/register/success");
+      reset();
+    },
+  });
+
+  const handleRegister = (data: IRegister) => {
+    mutateRegister(data);
+  };
+
+  return {
+    visiblePassword,
+    handleVisiblePassword,
+    control,
+    handleSubmit,
+    handleRegister,
+    errors,
+    isPendingRegister,
+  };
 };
 
 export default useRegister;
---
+++ src/pages/auth/register/success/index.tsx
@@ -0,0 +1,11 @@
+import AuthLayout from "@/components/layouts/AuthLayouts";
+
+const RegisterSuccessPage = () => {
+  return (
+    <AuthLayout title="Eventure | Register Success">
+      <h1>Register Success</h1>
+    </AuthLayout>
+  );
+};
+
+export default RegisterSuccessPage;
