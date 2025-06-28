98f7dddffc6811d434ecfe8b44bddbc376402f82
christ-saragih <bennefit.19@gmail.com>
2025-06-28 19:54:43 +0700

    feat: add activation page and success/failed illustrations

---
+++ src/pages/auth/activation/index.tsx
@@ -0,0 +1,46 @@
+import AuthLayout from "@/components/layouts/AuthLayouts";
+import Activation from "@/components/views/Auth/Activation";
+import authServices from "@/services/auth.service";
+
+interface PropTypes {
+    status: 'success' | 'failed';
+}
+
+const ActivationPage = (props: PropTypes) => {
+
+  return (
+    <AuthLayout title="Eventure | Activation">
+      <Activation {...props} />
+    </AuthLayout>
+  );
+};
+
+// server side rendering
+export async function getServerSideProps(context: { query: { code: string } }) {
+  try {
+    const result = await authServices.activation({ code: context.query.code });
+    console.log(result.data.data);
+    
+    if (result.data.data) {
+      return {
+        props: {
+          status: "success",
+        },
+      };
+    } else {
+      return {
+        props: {
+          status: "failed",
+        },
+      };
+    }
+  } catch (error) {
+    return {
+      props: {
+        status: "failed",
+      },
+    };
+  }
+}
+
+export default ActivationPage;
