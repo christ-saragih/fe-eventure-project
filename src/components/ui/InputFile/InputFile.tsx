2f7e28106567d004f0b1e036ff30851999da8238
christ-saragih <bennefit.19@gmail.com>
2025-10-12 16:31:18 +0700

    feat: implement detail category view with icon and info tabs

--- src/components/ui/InputFile/InputFile.tsx
+++ src/components/ui/InputFile/InputFile.tsx
@@ -1,7 +1,7 @@
 import { cn } from "@/utils/cn";
 import { Button, Spinner } from "@nextui-org/react";
 import Image from "next/image";
-import { ChangeEvent, useEffect, useId, useRef, useState } from "react";
+import { ChangeEvent, ReactNode, useEffect, useId, useRef, useState } from "react";
 import { CiSaveUp2, CiTrash } from "react-icons/ci";
 
 interface PropTypes {
@@ -11,6 +11,7 @@ interface PropTypes {
   isDropable?: boolean;
   isInvalid?: boolean;
   isUploading?: boolean;
+  label?: ReactNode;
   name: string;
   onUpload?: (files: FileList) => void;
   onDelete?: () => void;
@@ -25,6 +26,7 @@ const InputFile = (props: PropTypes) => {
     isInvalid,
     isDeleting,
     isUploading,
+    label,
     name,
     onUpload,
     onDelete,
@@ -70,6 +72,7 @@ const InputFile = (props: PropTypes) => {
 
   return (
     <div>
+      {label}
       <label
         ref={drop}
         htmlFor={`dropzone-file-${dropzoneId}`}
--- src/services/category.service.ts
+++ src/services/category.service.ts
@@ -5,9 +5,9 @@ import { ICategory } from "@/types/Category";
 const categoryService = {
   getCategories: (params?: string) =>
     instance.get(`${endpoint.CATEGORY}?${params}`),
+  getCategoryById: (id: string) => instance.get(`${endpoint.CATEGORY}/${id}`),
   addCategory: (payload: ICategory) =>
     instance.post(`${endpoint.CATEGORY}`, payload),
-
   deleteCategory: (id: string) => instance.delete(`${endpoint.CATEGORY}/${id}`),
 };
 
